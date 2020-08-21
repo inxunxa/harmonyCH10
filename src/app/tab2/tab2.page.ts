2d73dc1fa17e968d55340deb319f2d8924fc89ef
Sergio Inzunza <contact@sinzunza.com>
2020-08-20 19:28:54 -0700

    Initial commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  message = "";
+  imageUrl = "";
 
   constructor() {}
+
+  onPost() {
+    console.log("posting", this.message, this.imageUrl);
+  }
 
 }
